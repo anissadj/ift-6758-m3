01e447a58e638d1cdc60dc08628d785f5852a07b
D1amondb0t <d1amondb0t@outlook.com>
2025-11-27 11:13:22 -0500

    made changes to file nomenclature & fixed testing_serving_client json_data

--- ift6758/ift6758/client/testing_serving_client.py
+++ ift6758/ift6758/client/testing_serving_client.py
@@ -2,48 +2,55 @@ import pandas as pd
 from serving_client import ServingClient
 
 def test_predict():
-    print("Testing the prediction")
+    print("Testing /predict endpoint...")
+    
     client = ServingClient(ip="localhost", port=5000)
     df = pd.DataFrame([
         {"distance_to_net": 1},
         {"distance_to_net": 25},
         {"distance_to_net": 40}
     ])
+    
     response = client.predict(df)
+    
     print("Status Code:", response.status_code)
     try:
         print("Response JSON:", response.json())
     except:
         print("Raw Response:", response.text)
+    
+    return response.status_code == 200
 
 
 def test_logs():
-    print("Testing the logs")
+    print("Testing /logs endpoint...")
     client = ServingClient(ip="localhost", port=5000)
     try:
-        result = client.logs()
+        _ = client.logs()
         print("Logs have been loaded")
+        return True
     except Exception as e:
         print("Hitting an error calling logs")
         return False 
 
 
 def test_download_registry_model():
-    print('testing downloaiding the registy')
+    print('testing /download_registry_model endpoint...')
 
     client = ServingClient(ip="localhost", port=5000)
-
-    # Use dummy values unless your server really connects to Comet
-    entity =  "IFT6758-2025-B1",
+    
+    entity = "IFT6758-2025-B1",
     project = "IFT6758-2025-B01",
-    model_name =  "model1-distance",
-    version =  "latest"
+    model_name = "model1-distance",
+    version = "latest"
 
     try:
         result = client.download_registry_model(entity=entity, project=project, model_name=model_name, version=version)
         print("Registry Download Response:", result)
+        return True
     except Exception as e:
         print("Error calling /download_registry_model:", e)
+        return False
 
 
 if __name__ == "__main__":
@@ -56,6 +63,4 @@ if __name__ == "__main__":
     if (prediction and logs and download): 
         print("\n====== ALL TESTS COMPLETED WITH SUCCESS ======")
     else:
-        print("\n====== There was an error in one of our test ======")
-
-
+        print("\n====== There was an error in one of the tests ======")
